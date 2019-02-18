784de54712898944f2255d86e4156d9356c3d49b
Marcelo Matsumoto <marcelokmats@gmail.com>
2019-02-18 11:12:09 -0300

    carregamento de favoritos

--- app/src/main/java/com/marcelokmats/movilenext3_android_marcelomatsumto/util/LiveDataUtil.kt
+++ app/src/main/java/com/marcelokmats/movilenext3_android_marcelomatsumto/util/LiveDataUtil.kt
@@ -1,9 +1,24 @@
 package com.marcelokmats.movilenext3_android_marcelomatsumto.api
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+
+fun <T> LiveData<T>.observeOnce(observer: Observer<T>) {
+    observeForever(object : Observer<T> {
+        override fun onChanged(t: T?) {
+            observer.onChanged(t)
+            removeObserver(this)
+        }
+    })
+}
+
+/**
+ * https://gist.github.com/magneticflux-/044c9d7a3cea431aa0e4f4f4950a2898
+ */
+fun <A, B> zipLiveData()
 
 class RetrofitLiveData<T>
     (private val call: Call<T>) : LiveData<T>(), Callback<T> {
